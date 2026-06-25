f03a520e481e1a8c64cb515ed274912deb4f6c2e
shahidmomin102938-alt <shahidmomin102938@gmail.com>
2026-06-25 13:57:13 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'mt-belts-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).catch(() => cached);
+    })
+  );
+});
